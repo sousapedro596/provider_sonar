3aab3ef3d812136396bb5c0990213ad55fd38036
FMassey <francis.masse05@gmail.com>
2016-02-19 12:49:01 -0500

    include scanline_converter to main.cc

--- src/provider_sonar/main.cc
+++ src/provider_sonar/main.cc
@@ -2,12 +2,14 @@
 // ROS includes
 #include "ros/ros.h"
 #include "provider_sonar/provider_sonar_node.h"
+#include "provider_sonar/scanline_converter.h"
 
 int main(int argc, char **argv) {
   ros::init(argc, argv, "micron_driver");
 
   ros::NodeHandle nh("~");
   provider_sonar::ProviderSonarNode tritech_micron(nh);
+  provider_sonar::ScanLineConverter scanline_converter(nh);
 
   ros::spin();
 
